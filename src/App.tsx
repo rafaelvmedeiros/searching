06a33fb077dc841b19aa3903c953d0d2f1a542be
rafaelvmedeiros <rafaeldesenv.net@gmail.com>
2021-07-07 15:07:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <div>filter goes here</div>
+        <div>datatable goes here</div>
+      </div>
+  );
+}
+
+export default App;
