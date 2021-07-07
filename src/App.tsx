0306b63f7d1cd824046334fdf6216f63dfa1a273
rafaelvmedeiros <rafaeldesenv.net@gmail.com>
2021-07-07 15:13:59 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-      <div>
-        <div>filter goes here</div>
-        <div>datatable goes here</div>
-      </div>
+    <div>
+      <div>filter goes here</div>
+      <div>datatable goes here</div>
+      <div>ss</div>
+    </div>
   );
 }
 
